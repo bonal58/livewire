4b0ae958d483f300053335d604be830cada63848
berkay <b.onal@xeniox.nl>
2023-09-18 16:04:49 +0200

    livewire project 1 todo list

---
+++ livewireproject/resources/views/livewire/todo-list.blade.php
@@ -0,0 +1,17 @@
+<div>
+    @include('livewire.includes.create-todo-box')
+    @include('livewire.includes.search-box')
+        
+        <div id="todos-list">
+            @foreach($todos as $todo)
+             @include('livewire.includes.todo-card')
+            @endforeach
+
+            </div>
+
+            <div class="my-2">
+               {{ $todos->links()}}
+            </div>
+        </div>
+    </div>
+</div>
